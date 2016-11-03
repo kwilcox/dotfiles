17d34abc9657a188d9360422da69e1306607b280
Kyle Wilcox <kyle@axiomalaska.com>
2016-11-03 10:55:18 -0400

    Add a few more docker related shells

--- zsh/.oh-my-zsh/custom/docker.zsh
+++ zsh/.oh-my-zsh/custom/docker.zsh
@@ -1,3 +1,4 @@
+alias dk='docker'
 # Kill all running containers.
 alias dockerkillall='docker kill $(docker ps -q)'
 # Delete all stopped containers.
@@ -8,4 +9,12 @@ alias dockercleani='printf "\n>>> Deleting untagged images\n\n" && docker rmi $(
 dockerclean() {
     dockercleanc
     dockercleani
+}
+
+dkb() {
+  if [ ! -z "$1" ]; then
+    docker exec -it $1 bash
+  else
+    echo "Provide container name as a parameter"
+  fi
 }
