c0d9ca5ca3e1c5e1910323167fcd628cb7aec6fa
Kyle Wilcox <kyle@axiomdatascience.com>
2018-01-22 10:21:14 -0500

    Move xrandr config to a separate file, remove background execs

---
+++ bin/bin/xrandr_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xrandr --output DP-4 --rotate normal
+xrandr --output DP-0 --rotate right
+xrandr --output DP-2 --rotate left
+ 
+xrandr --output DP-4 --primary
+xrandr --output DP-0 --left-of DP-4
+xrandr --output DP-2 --right-of DP-4
