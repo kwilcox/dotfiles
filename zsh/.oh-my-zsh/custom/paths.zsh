523611be3874ae4c062f2fda5ec2a643e005f290
Kyle Wilcox <kyle@axiomdatascience.com>
2018-04-20 11:20:35 -0400

    Some .zsh config to compile wild programs

--- zsh/.oh-my-zsh/custom/paths.zsh
+++ zsh/.oh-my-zsh/custom/paths.zsh
@@ -3,3 +3,14 @@
 export PATH="/home/kwilcox/bin:$PATH"
 export PATH="/opt/sublime_text:$PATH"
 export PATH="/data/conda/miniconda3-py36/bin:$PATH"
+export PATH="/home/kwilcox/go/bin:$PATH"
+
+# Expand $PATH to include the directory where snappy applications go.
+if [ "${PATH#*/snap/bin}" = "${PATH}" ]; then
+    export PATH=$PATH:/snap/bin
+fi
+
+# For compilation of `expect` for the `unbuffer` command
+EXPECT_LIBS=/usr/lib/expect5.45.3
+export TCLLIBPATH=$EXPECT_LIBS
+export LD_LIBRARY_PATH=$EXPECT_LIBS:$LD_LIBRARY_PATH
