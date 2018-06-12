9383b42d72b5dadf815b8fbdce1d0aa06763a923
Kyle Wilcox <kyle@axiomdatascience.com>
2018-06-12 11:45:50 -0400

    Sublime font update and commit xrand command for octo

--- bin/bin/xrandr_config.sh
+++ bin/bin/xrandr_config.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Work 8910 Desktop
 xrandr --output DP-4 --rotate normal
 xrandr --output DP-0 --rotate right
 xrandr --output DP-2 --rotate left
- 
 xrandr --output DP-4 --primary
 xrandr --output DP-0 --left-of DP-4
 xrandr --output DP-2 --right-of DP-4
+
+# Home 8930 (octo)
+xrandr --output DP-0 --primary
