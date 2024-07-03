e8fbc6db588e9d7d4653bdfb4340bb939229824f
Kyle Wilcox <kyle@metify.io>
2024-07-03 12:11:18 -0400

    feat: add zsh auto suggest and syntax highlighting

---
+++ zsh/.oh-my-zsh/custom/zsh-auto-suggestions.zsh
@@ -0,0 +1,3 @@
+# sudo apt-get install zsh-autosuggestions
+AUTO_SUGGEST_PATH="/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+[ -f "$AUTO_SUGGEST_PATH" ] && source "$AUTO_SUGGEST_PATH"
---
+++ zsh/.oh-my-zsh/custom/zsh-syntax-highlighting.zsh
@@ -0,0 +1,3 @@
+# sudo apt-get install zsh-syntax-highlighting
+SYN_HIGHTLIGHT_PATH="/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+[ -f "$SYN_HIGHTLIGHT_PATH" ] && source "$SYN_HIGHTLIGHT_PATH"
